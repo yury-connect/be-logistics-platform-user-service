3080a6da3cda2ec1ad46b31114355369f90976c4
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-05-15 12:22:54 +0300

    user-service: init - commit пока не стартует, я так понимаю из-за БД

---
+++ src/main/java/com/innowise/logistics/userservice/BeLogisticsPlatformUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.logistics.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeLogisticsPlatformUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeLogisticsPlatformUserServiceApplication.class, args);
+	}
+
+}
